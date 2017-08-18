2d96ef5d3ebed691ecf5ea7a5f3e3c0abf6570f2
ryonakao <rodriguez.nak@gmail.com>
2017-08-18 16:38:33 +0900

    mainスレッドでのチャットルームの生成

--- main.go
+++ main.go
@@ -23,8 +23,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	// ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルーム開始
+	go r.run()
 
 	// webサーバーを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
