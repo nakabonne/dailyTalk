40fab388ec6db816b171efd7133a8729711b14c7
ryonakao <rodriguez.nak@gmail.com>
2017-08-20 12:29:20 +0900

    ポートを指定できるように

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 	go r.run()
 	// webサーバーを開始
 	log.Println("webサーバーを開始します。ポート：", *addr)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndSearver:", err)
 	}
 }
